ae7fa3db7523792bd67998463b07e78a7e8a0a33
Camila Boaventura <camilamboaventura@gmail.com>
2021-04-06 18:43:52 +0200

    first obstacle was created

--- js/index.js
+++ js/index.js
@@ -19,8 +19,8 @@ class GameObject {
       this.y = this.height + 160;
     }
 
-    if (this.y >= canvas.height - this.height) {
-      this.y = canvas.height - this.height;
+    if (this.y >= canvas.height - (this.height - 10)) {
+      this.y = canvas.height - (this.height - 10);
     }
 
     this.x += this.speedX;
@@ -49,11 +49,13 @@ class BackgroundImage extends GameObject {
 }
 
 class Game {
-  constructor(background, player) {
+  constructor(background, player, boatObstacleImg) {
     this.background = background;
     this.player = player;
+    this.boatObstacleImg = boatObstacleImg;
     this.frames = 0;
     this.animationID;
+    this.obstacles = [];
   }
 
   start = () => {
@@ -69,7 +71,35 @@ class Game {
     this.player.updatePosition();
     this.player.draw();
 
+    this.updateObstacles();
+
     this.animationID = requestAnimationFrame(this.updateGame);
+  };
+
+  updateObstacles = () => {
+    this.frames++;
+
+    for (let i = 0; i < this.obstacles.length; i++) {
+      this.obstacles[i].updatePosition();
+      this.obstacles[i].draw();
+    }
+
+    if (this.frames % 240 === 0) {
+      const originX = 500;
+
+      const minY = 300;
+      const maxY = 580;
+      const randomY = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
+
+      const obstacle = new GameObject(
+        originX,
+        randomY,
+        150,
+        60,
+        this.boatObstacleImg
+      );
+      this.obstacles.push(obstacle);
+    }
   };
 
   clear = () => {
@@ -78,11 +108,15 @@ class Game {
 }
 
 function startGame() {
+  //instanciando as imagens
   const bgImg = new Image();
   bgImg.src = "./images/amsterdam_canal3.png";
 
   const boatPlayer = new Image();
   boatPlayer.src = "./images/boat_player2.png";
+
+  const boatObstacle = new Image();
+  boatObstacle.src = "./images/boat_obstacle.png";
 
   const backgroundImage = new BackgroundImage(
     0,
@@ -92,11 +126,13 @@ function startGame() {
     bgImg
   );
 
+  //   const boatobstacle = new GameObject(canvas.width, canvas.height, 150, 60, obstacle1)
   const player = new GameObject(canvas.width - 780, 390, 170, 100, boatPlayer);
-  const game = new Game(backgroundImage, player);
+  const game = new Game(backgroundImage, player, boatObstacle);
 
   game.start();
 
+  //definindo setas para cima e para baixo como teclas para movimentar o barco
   document.addEventListener("keydown", (event) => {
     if (event.code === "ArrowUp") {
       game.player.speedY = -2;
@@ -105,11 +141,13 @@ function startGame() {
     }
   });
 
+  //freiando o barco quando as teclas setas para cima e para baixo não estão pressionadas
   document.addEventListener("keyup", () => {
     game.player.speedY = 0;
   });
 }
 
+//carregando o jogo e iniciando com o botão start
 window.onload = () => {
   document.getElementById("start-button").onclick = () => {
     startGame();
