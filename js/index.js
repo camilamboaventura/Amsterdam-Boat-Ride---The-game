493f3b3ad98d76e6eff53add45582c9f8564a573
Camila Boaventura <camilamboaventura@gmail.com>
2021-04-09 16:47:09 +0200

    last adjustments

--- js/index.js
+++ js/index.js
@@ -39,8 +39,6 @@ const sup = new Image();
 sup.src = "./images/sup.png";
 
 
-//   const boatobstacle = new GameObject(canvas.width, canvas.height, 150, 60, obstacle1)
-
 
 class GameObject {
   constructor(x, y, width, height, img) {
@@ -54,7 +52,7 @@ class GameObject {
   }
 
   updatePosition() {
-    this.y += this.speedY;
+    this.y += this.speedY; //atualiza a posição do barco e dos obstáculos
 
     if (this.y <= this.height + 210) {
       this.y = this.height + 210;
@@ -67,7 +65,7 @@ class GameObject {
     this.x += this.speedX;
   }
 
-  draw() {
+  draw() { //desenha as imagens no canvas
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
 
@@ -84,7 +82,7 @@ class GameObject {
     return this.y + this.height;
   }
 
-  crashWith(obstacle) {
+  crashWith(obstacle) { //determina as possibilidades de colisão
     return !(
       this.bottom() < obstacle.top() ||
       this.top() > obstacle.bottom() ||
@@ -97,22 +95,22 @@ class GameObject {
 class BackgroundImage extends GameObject {
   constructor(x, y, width, height, img) {
     super(x, y, width, height, img);
-    this.speedX = - 4;
+    this.speedX = - 3;
   }
 
-  updatePosition() {
+  updatePosition() { //atualiza a posição da imagem de fundo incrementando a velocidade
     this.x += this.speedX;
     this.x %= canvas.width;
   }
 
-  draw() {
+  draw() { //a imagem de fundo é desenhada duas vezes para que as duas se unam, funcionem como uma esteira, mas se parecem como uma única imagem na tela
     ctx.drawImage(this.img, this.x, 0, this.width, this.height);
     ctx.drawImage(this.img, this.x + canvas.width, 0, this.width, this.height);
   }
 
 }
 
-class Game {
+class Game { 
   constructor(background, player) {
     this.background = background;
     this.player = player;
@@ -127,7 +125,7 @@ class Game {
     this.updateGame();
   };
 
-  updateGame = () => {
+  updateGame = () => { //determina a ordem do funcionamento do jogo; atualiza as sequências de açōes determinadas por cada método.
     this.clear();
 
     this.background.updatePosition();
@@ -140,7 +138,7 @@ class Game {
 
     this.updateScore();
 
-    this.animationID = requestAnimationFrame(this.updateGame);
+    this.animationID = requestAnimationFrame(this.updateGame); //faz os objetos de movimentarem("animarem")
 
     this.checkGameOver();
   };
@@ -148,20 +146,20 @@ class Game {
   updateObstacles = () => {
     this.frames++;
 
-    for (let i = 0; i < this.obstacles.length; i++) {
-     this.obstacles[i].speedX = - 5;
-        this.obstacles[i].updatePosition();
-      this.obstacles[i].draw();
+    for (let i = 0; i < this.obstacles.length; i++) { //percorre o array de obstáculos
+     this.obstacles[i].speedX = - 3; //velocidade dos obstáculos
+        this.obstacles[i].updatePosition(); //atualiza a posição dos obstáculos
+      this.obstacles[i].draw(); //desenha 
     }
 
-    if (this.frames % 100 === 0) {
+    if (this.frames % 120 === 0) { //frequência pela qual os obstáculos aparecerem na tela
       const randomObstacle = Math.floor(Math.random() * this.obstaclesImages.length)
       
-        const originX = 750;
+        const originX = 750; //ponto de origem no eixo x
 
       const minY = 300;
       const maxY = 580;
-      const randomY = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
+      const randomY = Math.floor(Math.random() * (maxY - minY + 1)) + minY; //determina o aparecimento randômico dos obstáculos
 
       const imageRandom =  this.obstaclesImages[randomObstacle]
       const obstacle = new GameObject(
@@ -171,34 +169,34 @@ class Game {
         imageRandom.height,
         imageRandom
       );
-      this.obstacles.push(obstacle);
+      this.obstacles.push(obstacle); //add no array de obstáculos
 
-      this.score++
+      this.score++ //incrementa a pontuação do jogo
     }
   };
 
   checkGameOver = () => {
-    const crashed = this.obstacles.some((obstacle) => {
+    const crashed = this.obstacles.some((obstacle) => { //verifica se houve a colisão
       return this.player.crashWith(obstacle);
     });
 
-    if (crashed) {
+    if (crashed) { //se houve colisão a música para de tocar, toca o audio do game over
      gameMusic.pause()
      gameOverSound.play()
 
-      cancelAnimationFrame(this.animationID);
+      cancelAnimationFrame(this.animationID); //para as animações
 
-      this.gameOver();
+      this.gameOver(); //chama o método
     }
   };
 
-  updateScore() {
+  updateScore() { //mostra a pontuação no canvas nas coordenadas determinadas abaixo 
     ctx.font = "30px Verdana";
     ctx.fillStyle = "white";
     ctx.fillText(`Score: ${this.score}`, 50, 40);
   }
 
-  gameOver() {
+  gameOver() { //tela do game over
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#ffc68c";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -214,12 +212,12 @@ class Game {
     reloadButton.style.display = "flex";
   }
 
-  clear = () => {
+  clear = () => { //limpa a tela do canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   };
 }
 
-function startGame() {
+function startGame() { //inicia o jogo
   
     const backgroundImage = new BackgroundImage(
         0,
@@ -236,11 +234,16 @@ const game = new Game(backgroundImage, player, boatObstacle);
 
   //definindo setas para cima e para baixo como teclas para movimentar o barco
   document.addEventListener("keydown", (event) => {
+    if(["ArrowUp","ArrowDown"].indexOf(event.code) > -1) { //as setas não serão utilizadas para rolagem da página
+      event.preventDefault();
+}
     if (event.code === "ArrowUp") {
       game.player.speedY = -3;
     } else if (event.code === "ArrowDown") {
       game.player.speedY = 3;
     }
+
+  
   });
 
   //freiando o barco quando as teclas setas para cima e para baixo não estão pressionadas
@@ -250,7 +253,7 @@ const game = new Game(backgroundImage, player, boatObstacle);
 }
 
 //carregando o jogo e iniciando com o botão start
-window.onload = () => {
+window.onload = () => { 
   document.getElementById("start-button").onclick = () => {
     container.style.display = "none";
     gameMusic.play();
@@ -259,7 +262,7 @@ window.onload = () => {
   };
 
 
-
+//botão que reinicia o jogo
   reloadButton.onclick = () => {
     document.location.reload(true);
   }
